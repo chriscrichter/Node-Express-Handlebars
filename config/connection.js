f7623399bf14a12bffa2760f57fb4abfa52c71c1
chriscrichter <chris_richter@outlook.com>
2020-02-13 14:11:10 -0500

    Updated js file.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 //if (process.env.JAWSDB_URL) {
-    //connection = mysql.createConnection(process.env.JAWSDB_URL);
+    //connection = mysql.createConnection(process.env.JAWSDB_COPPER_URL);
 
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_COPPER_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
